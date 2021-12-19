63e562077eabd481520c9000e4ee1fab5eacdc0c
hobee_mac <lzqpublic@163.com>
2021-12-19 19:57:31 +0800

    完成文本ir后端接入

--- BiliLoader/views.py
+++ BiliLoader/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse_lazy
 
+from model.BM25.core import bm25f
+
 # Create your views here.
 
 ctx = {}
@@ -20,6 +22,15 @@ def predict(request):
 
 
 def text_ir(request):
+    keyword = request.GET.get('search_for', None)
+    title_weight = request.GET.get('title_weight', None)
+    if not title_weight:
+        title_weight = '0.8'
+    if not keyword:
+        return render(request, 'text-ir.html', context=ctx)
+    title_weight = float(title_weight)
+    res = bm25f(keyword, v1=title_weight, v2=1 - title_weight)
+    print(res)
     return render(request, 'text-ir.html', context=ctx)
 
 
